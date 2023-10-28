39d1ce8acbfa92d29f66d21985c9b88c561fb1fd
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-10-28 12:18:54 -0400

    added migrations

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,14 +1,30 @@
-from flask import Flask 
+# config                    
+from flask import Flask
+from flask_migrate import Migrate 
 
-def create_app(): 
+# factory
+def create_app():
     app = Flask(__name__)
 
-    # register pet blueprint 
-    from . import pet
-    app.register_blueprint(pet.bp)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False     
+    
+    from . import models 
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
+    # index route
     @app.route('/')
-    def hello(): 
+    def index(): 
         return 'Hello, PetFax!'
 
+    # register pet blueprint 
+    from . import pet 
+    app.register_blueprint(pet.bp)
+
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    # return the app 
     return app
